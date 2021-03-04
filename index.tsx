ba5fe0a7eff27478cc269157f2d5ab64eec7592a
Ming-der Wang <mingderwang@gmail.com>
2021-03-04 22:32:27 +0800

    init

---
+++ index.tsx
@@ -0,0 +1,11 @@
+// index.tsx
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+console.log('Hello from tsx!')
+
+ReactDOM.render(
+  <p>Hello</p>,
+  document.getElementById('root'),
+)
