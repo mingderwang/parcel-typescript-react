06ea7345b926d7c8ff2c5550f184197c94552212
Ming-der Wang <mingderwang@gmail.com>
2021-03-04 23:28:11 +0800

    use search button

--- App.tsx
+++ App.tsx
@@ -1,25 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
  
 function App() {
   const [data, setData] = useState({ hits: [] });
-  useEffect(async () => {
-    const result = await axios(
-      'https://hn.algolia.com/api/v1/search?query=redux',
-    );
+  const [query, setQuery] = useState('redux');
+  const [search, setSearch] = useState('');
  
-    setData(result.data);
-  });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        `http://hn.algolia.com/api/v1/search?query=${query}`,
 
+      );
+ 
+      setData(result.data);
+    };
+ 
+    fetchData();
+  }, [search]);
+ 
   return (
-    <h1>test</h1>
-    <ul>
-      {data.hits.map(item => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <Fragment>
+      <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+      />
+      <button type="button" onClick={() => setSearch(query)}>
+        Search
+      </button>
+      <ul>
+        {data.hits.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
  
