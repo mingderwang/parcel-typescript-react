179d18bdc946f03f1a17904714959aaa6b3dd929
Ming-der Wang <mingderwang@gmail.com>
2021-03-04 23:22:57 +0800

    add useEffect

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from 'react'
-import UseEffectComponent from "./UseEffectComponent"
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ 
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  useEffect(async () => {
+    const result = await axios(
+      'https://hn.algolia.com/api/v1/search?query=redux',
+    );
+ 
+    setData(result.data);
+  });
 
-export default function App() {
-  const [count, setCount] = useState<number>(1);
-  console.log('ming')
   return (
-    <div>
-      <h1>Hello Ming {count}</h1>
-      <UseEffectComponent/>
-    </div>
+    <h1>test</h1>
+    <ul>
+      {data.hits.map(item => (
+        <li key={item.objectID}>
+          <a href={item.url}>{item.title}</a>
+        </li>
+      ))}
+    </ul>
   );
 }
+ 
+export default App;
